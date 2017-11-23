e5b9490d965d47290845f9a45f5a3f3bb156eaef
bbalconi <bbalconi@gmail.com>
2017-11-23 11:43:51 -0700

    heroku test

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import openSocket from 'socket.io-client';
+import { InputGroup, InputGroupButton, Input, Button } from 'reactstrap';
+import './App.css';
+var axios = require('axios')
+
+
+class App extends Component {
+  constructor(){
+    super()
+    this.nameChange = this.nameChange.bind(this)
+    this.thanksChange = this.thanksChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      name: '',
+      thanks: '',
+    }
+  }
+
+  nameChange(event){
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  thanksChange(event){
+    this.setState({
+      thanks: event.target.value
+    })
+  }
+
+  handleClick(){
+
+  }
+
+  componentDidMount(){
+    axios.post('/socketUrl').then((res) => {
+      var socketUrl = res.data;
+      this.socket = openSocket(socketUrl)
+      console.log(this.socket)
+      this.socket.emit('thanksConnect')
+  })
+}
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className='bg'>
+         <Input placeholder='Enter your name...' className='lol' onChange={this.nameChange}/>
+         <Input placeholder="Enter what you're thankful for..." onChange={this.thanksChange}/>
+         <Button className='thisbutton' color="secondary" onClick={this.handleClick}>Give Thanks!</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var http = require('http');
+var app = require("express")();
+var server = require('http').Server(app);
+var io = require('socket.io');
+let bodyParser = require("body-parser");
+var fetch = require('node-fetch');
+var cookieParser = require('cookie-parser');
+var path  = require('path');
+// var fs = require('fs');
+
+
+ 
+var allowedOrigins = "http://localhost:* http://192.168.*.*:* https://coffee-pot-pi.herokuapp.com:*";
+var ioServer = io(server, {
+  origins: allowedOrigins
+});
+ 
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./client/build'));
+} else {
+  app.use(express.static('public'));
+}
+
+
+  ioServer.on('connection', (client) => {
+    console.log('client connected! id: ', client.id)
+
+
+    client.on('piDisconnected', ()=>{
+      console.log('FUCKKKKKK')
+    });
+
+    client.on('thanksConnect', (data) => {
+      client.join(data);
+    });
+
+    client.on('/messageSend', (data) => {
+
+    })
+
+    client.on('disconnect', ()=>{console.log("client disconnected")});
+  });
+
+app.post('/socketUrl', (req, res)=>{
+  if (process.env.PORT){
+    res.json('https://coffee-pot-pi.herokuapp.com:');
+  } else {
+    res.json('http://localhost:5000')
+  }
+});
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+var port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log('listening on port ' + port);
+});    
